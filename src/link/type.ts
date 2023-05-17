47ed771d5d930dd8b7c16c322e5cab856b8cb777
efen stakes <efenstakes101@gmail.com>
2023-05-17 14:36:51 +0300

    add link graphql type

---
+++ src/link/type.ts
@@ -0,0 +1,56 @@
+import { GraphQLBoolean } from 'graphql'
+import {
+    GraphQLString,
+    GraphQLID,
+    GraphQLObjectType,
+    GraphQLFloat,
+} from 'graphql'
+
+
+// import models here
+
+// types
+
+
+const LinkType = new GraphQLObjectType({
+    name: 'LinkType',
+    description: 'Link Type',
+    fields: ()=> ({
+        _id: {
+            type: GraphQLID
+        },
+        chainId: {
+            type: GraphQLString
+        },
+        name: {
+            type: GraphQLString
+        },
+        aka: {
+            type: GraphQLString
+        },
+        universe: {
+            type: GraphQLString
+        },
+        link: {
+            type: GraphQLString
+        },
+        bio: {
+            type: GraphQLString
+        },
+        avatar: {
+            type: GraphQLString,
+        },
+        bgAvatar: {
+            type: GraphQLString,
+        },
+        active: {
+            type: GraphQLBoolean,
+        },
+        addedOn: {
+            type: GraphQLFloat,
+        },
+        
+    })
+})
+
+export default LinkType
