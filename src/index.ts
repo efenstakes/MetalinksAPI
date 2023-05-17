061f1ffc27c0f9fdac89598dbf498c6990d82e2e
efen stakes <efenstakes101@gmail.com>
2023-05-17 15:15:44 +0300

    add graphql schema and morgan

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 import express, { Express , Response, Request} from "express"
+import cors from 'cors'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import { graphqlHTTP } from 'express-graphql'
 
+
+// db connection
+import { connectToDb } from "./utils/database"
+
+
+// schema
+import schema from './schema'
+
+
+// create express server
 const app: Express = express()
 
+
+// connect to db
+connectToDb()
+
+
+// get environment variables
+dotenv.config({ path: './env' })
+
+
+// allow cross origin requests
+app.use(cors())
+
+
+// show dev logs
+app.use(morgan('dev'))
+
+
+console.log("env", process.env.DB_URL)
 
 app.get("/", (req: Request, res: Response)=> {
     res.json({ page: 'index', status: 'running' })
@@ -10,6 +42,14 @@ app.get("/", (req: Request, res: Response)=> {
 console.log("hola")
 
 
+// setup graqphql
+app.use(
+    '/q',
+    graphqlHTTP({ schema, graphiql: true })
+)
+
+
+// start server
 app.listen(8080, ()=> {
     console.log("server listening on port 8080")
 })
