1b9b8c50fe9a4e66b5181c19d5da82e5b77f29cc
efen stakes <efenstakes101@gmail.com>
2023-05-17 16:45:55 +0300

    update db logic

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -7,7 +7,7 @@ export const connectToDb = async() => {
     let connection
 
     try { // vicci
-        connection = await mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/metalinks', { autoIndex: false, })
+        connection = await mongoose.connect(process.env.DB_URL, { autoIndex: false, })
         console.log('connected to db')
     } catch (e) {
         console.log('error connecting to db ', e)
