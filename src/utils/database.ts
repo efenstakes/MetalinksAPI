3f0da681a7fb4387eb2e7ea7d10a1552676849a9
efen stakes <efenstakes101@gmail.com>
2023-05-20 12:23:28 +0300

    update db script

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,15 +3,19 @@ import mongoose from 'mongoose'
 
 mongoose.Promise = Promise
 
-export const connectToDb = async() => {
-    let connection
+mongoose.connection.on('connection', (_)=> {
+    console.log('connected to db')
+})
 
-    try { // vicci
-        connection = await mongoose.connect(process.env.DB_URL, { autoIndex: false, })
+mongoose.connection.on('error', (_)=> {
+    console.log('connected to db')
+})
+
+export const connectToDb = async() => {
+    try {
+        await mongoose.connect(process.env.DB_URL, { autoIndex: false, })
         console.log('connected to db')
     } catch (e) {
         console.log('error connecting to db ', e)
     }
-
-    return connection
 }
